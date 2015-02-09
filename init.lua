a86b6711bbad60d18a243071ca1018767872643e
Marcos Kirsch <bitbucket@kirsch.com.mx>
2015-02-08 22:03:44 -0600

    Cleaned up

--- init.lua
+++ init.lua
@@ -8,16 +8,19 @@ print('chip: ',node.chipid())
 print('heap: ',node.heap())
 wifi.sta.config("Internet","")
 
+--require("httpserver")
+
 -- Wait until WiFi connection is established
 
 tmr.alarm(0, 2000, 1, function()
    if wifi.sta.getip() == nil then
       print("Connecting to AP...")
    else
-      print('IP: ',wifi.sta.getip())
       tmr.stop(0)
+      print('IP: ',wifi.sta.getip())
+      local port = 80
+      --server = httpserver.start(port, 10)
+      --print("nodemcu-httpserver started on port " .. port)
    end
 end)
 
-
---dofile("garage.lua")
