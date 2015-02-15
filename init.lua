3f5caa7afb200ced24d5b356ebfb3c11a17b8208
Marcos Kirsch <bitbucket@kirsch.com.mx>
2015-02-15 16:48:30 -0600

    Clean up

--- init.lua
+++ init.lua
@@ -8,19 +8,12 @@ print('chip: ',node.chipid())
 print('heap: ',node.heap())
 wifi.sta.config("Internet","")
 
---require("httpserver")
-
--- Wait until WiFi connection is established
-
 tmr.alarm(0, 3000, 1, function()
    if wifi.sta.getip() == nil then
       print("Connecting to AP...")
    else
       tmr.stop(0)
       print('IP: ',wifi.sta.getip())
-      local port = 80
-      --server = httpserver.start(port, 10)
-      --print("nodemcu-httpserver started on port " .. port)
    end
 end)
 
