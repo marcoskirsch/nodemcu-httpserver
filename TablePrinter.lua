6ba7c0238124b2e5f800401ae3f3160393aafc8e
Marcos Kirsch <bitbucket@kirsch.com.mx>
2015-02-22 16:04:08 -0600

    Attempt at improving the printing, but will delete anyway

--- TablePrinter.lua
+++ TablePrinter.lua
@@ -1,32 +1,34 @@
 -- Print anything - including nested tables
 -- Based on but modified from:
 -- http://lua-users.org/wiki/TableSerialization
-module("TablePrinter", package.seeall)
+--module("TablePrinter", package.seeall)
 
-function TablePrinter.print (tt, indent, done)
+local function printTable(tt, indent, done)
    done = done or {}
    indent = indent or 0
    if tt == nil then
-      print("nil\n")
+      print("nil")
    else
       if type(tt) == "table" then
          for key, value in pairs (tt) do
             print(string.rep (" ", indent)) -- indent it
             if type (value) == "table" and not done [value] then
                done [value] = true
-               print(string.format("[%s] => table\n", tostring (key)));
+               print(string.format("[%s] => table", tostring (key)));
                print(string.rep (" ", indent+4)) -- indent it
-               print("(\n");
-               table_print (value, indent + 7, done)
+               print("(");
+               printTable (value, indent + 7, done)
                print(string.rep (" ", indent+4)) -- indent it
-               print(")\n");
+               print(")");
             else
-               print(string.format("[%s] => %s\n",
+               print(string.format("[%s] => %s",
                tostring (key), tostring(value)))
             end
          end
       else
-         print(tt .. "\n")
+         print(tt)
       end
    end
 end
+
+return printTable
