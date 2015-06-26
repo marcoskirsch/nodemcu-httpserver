d5dff7ae458270b14b8d6c63a1c8624998836b7c
olga <olga@olga-Aspire-S3>
2015-06-26 12:39:05 +0800

    remove the coroutine feature

--- httpserver-static.lua
+++ httpserver-static.lua
@@ -2,30 +2,31 @@
 -- Part of nodemcu-httpserver, handles sending static files to client.
 -- Author: Marcos Kirsch
 
-return function (connection, args)
-   dofile("httpserver-header.lc")(connection, 200, args.ext)
+return function (connection, args, ishead)
+   if ishead == 1 then
+      dofile("httpserver-header.lc")(connection, 200, args.ext)
+   end
+
    --print("Begin sending:", args.file)
    -- Send file in little chunks
-   local continue = true
-   local bytesSent = 0
-   while continue do
-      collectgarbage()
-      -- NodeMCU file API lets you open 1 file at a time.
-      -- So we need to open, seek, close each time in order
-      -- to support multiple simultaneous clients.
-      file.open(args.file)
-      file.seek("set", bytesSent)
-      local chunk = file.read(256)
-      file.close()
-      if chunk == nil then
-         continue = false
-      else
-         coroutine.yield()
-         connection:send(chunk)
-         bytesSent = bytesSent + #chunk
-         chunk = nil
-         --print("Sent" .. args.file, bytesSent)
-      end
+   local bytesSent = connectionTable[connection].bytesSent
+
+   collectgarbage()
+   -- NodeMCU file API lets you open 1 file at a time.
+   -- So we need to open, seek, close each time in order
+   -- to support multiple simultaneous clients.
+   file.open(args.file)
+   file.seek("set", bytesSent)
+   local chunk = file.read(256)
+   file.close()
+   if chunk == nil then
+      return 0
+   else
+      connection:send(chunk)
+      bytesSent = bytesSent + #chunk
+      connectionTable[connection].bytesSent = bytesSent
+      chunk = nil
+      --print("Sent" .. args.file, bytesSent)
+	  return 1
    end
-   --print("Finished sending:", args.file)
 end
