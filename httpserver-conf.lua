aa507264eba7548d8baa86ce9c7d3cb297936650
Marcos Kirsch <bitbucket@kirsch.com.mx>
2015-07-19 13:34:16 -0500

    Better comments, add realm

--- httpserver-conf.lua
+++ httpserver-conf.lua
@@ -1,20 +1,15 @@
 -- httpserver-conf.lua
--- Part of nodemcu-httpserver, contains static configuration for httpserver. 
+-- Part of nodemcu-httpserver, contains static configuration for httpserver.
 -- Author: Sam Dieck
 
-conf = {}
-
--- WIFI
--- FIXME use these
---wifi = {}
---wifi.essid = "Internet"
---wifi.password = ""
+local conf = {}
 
 -- Basic Authentication Conf
-auth = {}
-auth.enabled = false
+local auth = {}
+auth.enabled = true
+auth.realm = "nodemcu-httpserver" -- displayed in the login dialog users get
 auth.user = "user"
-auth.password = "password"
+auth.password = "password" -- PLEASE change this
 conf.auth = auth
 
 return conf
