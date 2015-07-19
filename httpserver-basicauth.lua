0bd5e20c62321db07857d06edf506e443e2a491d
Marcos Kirsch <bitbucket@kirsch.com.mx>
2015-07-19 13:33:47 -0500

    Retrofit for renamed file, display configurable realm

--- httpserver-basicauth.lua
+++ httpserver-basicauth.lua
@@ -9,21 +9,21 @@ function basicAuth.authenticate(header)
    -- Parse basic auth http header.
    -- Returns the username if header contains valid credentials,
    -- nil otherwise.
-   local credentials_enc = header:match("Authorization: Basic ([A-Za-z0-9+/=]+)") 
+   local credentials_enc = header:match("Authorization: Basic ([A-Za-z0-9+/=]+)")
    if not credentials_enc then
       return nil
    end
-   local credentials = dofile("b64.lc").decode(credentials_enc)
+   local credentials = dofile("httpserver-b64decode.lc")(credentials_enc)
    local user, pwd = credentials:match("^(.*):(.*)$")
    if user ~= conf.auth.user or pwd ~= conf.auth.password then
       return nil
-   end 
-   print("httpserver-basicauth: User " .. user .. " authenticated.")
+   end
+   print("httpserver-basicauth: User \"" .. user .. "\" authenticated.")
    return user
 end
 
 function basicAuth.authErrorHeader()
-   return "WWW-Authenticate: Basic realm=\"nodemcu-httpserver\""
+   return "WWW-Authenticate: Basic realm=\"" .. conf.auth.realm .. "\""
 end
 
 return basicAuth
