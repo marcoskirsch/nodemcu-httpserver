6cb87ffa5ba325bcb9d3c0123c428240069a47ec
moononournation <moononournation@gmail.com>
2016-05-27 22:24:09 +0800

    Revert "add editor.lua"
    This reverts commit a01f905ec9a90b9de3e785fb2be4241ae95199d3.

--- http/adhoc.lua
+++
@@ -1,6 +0,0 @@
-return function (connection, req, args)
-   dofile('httpserver-header.lc')(connection, 200, 'html')
-   connection:send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>lua editor</title></head><body>')
-   connection:send('<h1>Hello World!</h1>')
-   connection:send('</body></html>')
-end
