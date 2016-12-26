856ef7a590d061d55e96dc6c0df4f115c01577df
Marcos Kirsch <bitbucket@kirsch.com.mx>
2016-12-26 13:48:54 -0600

    There was a typo (entra n) when sending 'Cache-Control: private, no-store' in the header. I removed the whole thing because why not let browser cache?

--- httpserver-header.lua
+++ httpserver-header.lua
@@ -19,7 +19,7 @@ return function (connection, code, extension, isGzipped)
 
    local mimeType = getMimeType(extension)
 
-   connection:send("HTTP/1.0 " .. code .. " " .. getHTTPStatusString(code) .. "\r\nServer: nodemcu-httpserver\r\nContent-Type: " .. mimeType .. "\r\nnCache-Control: private, no-store\r\n")
+   connection:send("HTTP/1.0 " .. code .. " " .. getHTTPStatusString(code) .. "\r\nServer: nodemcu-httpserver\r\nContent-Type: " .. mimeType .. "\r\n")
    if isGzipped then
       connection:send("Cache-Control: private, max-age=2592000\r\nContent-Encoding: gzip\r\n")
    end
