203448067937bbbfb4cdc16f8c6305d955641fd0
Marcos Kirsch <github@kirsch.com.mx>
2017-01-01 22:12:18 -0600

    Simplify by using file object model API

--- httpserver-static.lua
+++ httpserver-static.lua
@@ -3,32 +3,22 @@
 -- Author: Marcos Kirsch
 
 return function (connection, req, args)
-   --print("Begin sending:", args.file)
-   --print("node.heap(): ", node.heap())
    dofile("httpserver-header.lc")(connection, 200, args.ext, args.isGzipped)
    -- Send file in little chunks
-   local continue = true
-   local size = file.list()[args.file]
-   local bytesSent = 0
+   local bytesRemaining = file.list()[args.file]
    -- Chunks larger than 1024 don't work.
    -- https://github.com/nodemcu/nodemcu-firmware/issues/1075
    local chunkSize = 1024
-   while continue do
-      collectgarbage()
-
-      -- NodeMCU file API lets you open 1 file at a time.
-      -- So we need to open, seek, close each time in order
-      -- to support multiple simultaneous clients.
-      file.open(args.file)
-      file.seek("set", bytesSent)
-      local chunk = file.read(chunkSize)
-      file.close()
-
+   fileHandle = file.open(args.file)
+   while bytesRemaining > 0 do
+      local bytesToRead = 0
+      if bytesRemaining > chunkSize then bytesToRead = chunkSize else bytesToRead = bytesRemaining end
+      local chunk = fileHandle:read(bytesToRead)
       connection:send(chunk)
-      bytesSent = bytesSent + #chunk
+      bytesRemaining = bytesRemaining - #chunk
       chunk = nil
-      --print("Sent: " .. bytesSent .. " of " .. size)
-      if bytesSent == size then continue = false end
+      collectgarbage()
    end
-   --print("Finished sending: ", args.file)
+   print("Finished sending: ", args.file)
+   fileHandle:close()
 end
