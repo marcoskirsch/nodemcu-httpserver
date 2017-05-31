addda8dbe7e4669c4a860f16e5115618fdc61556
Gregor <grex@gmx.de>
2017-05-31 22:27:29 +0200

    use correct code for erropage instead of failing
    Sorry I didn't see this earlier

--- httpserver-error.lua
+++ httpserver-error.lua
@@ -3,6 +3,7 @@
 -- Author: Marcos Kirsch, Gregor Hartmann
 
 return function (connection, req, args)
+   req.code = args.code
    local statusString = dofile("httpserver-header.lc")(connection, req.code, "html", false, req.headers)
    connection:send("<html><head><title>" .. req.code .. " - " .. statusString .. "</title></head><body><h1>" .. req.code .. " - " .. statusString .. "</h1></body></html>\r\n")
 end
