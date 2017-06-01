c44deb097c27f84c8ec93984b4ac9dcfe0e318b9
Gregor <grex@gmx.de>
2017-06-01 04:36:29 +0200

    using args.code now

--- httpserver-error.lua
+++ httpserver-error.lua
@@ -3,7 +3,6 @@
 -- Author: Marcos Kirsch, Gregor Hartmann
 
 return function (connection, req, args)
-   req.code = args.code
-   local statusString = dofile("httpserver-header.lc")(connection, req.code, "html", false, req.headers)
-   connection:send("<html><head><title>" .. req.code .. " - " .. statusString .. "</title></head><body><h1>" .. req.code .. " - " .. statusString .. "</h1></body></html>\r\n")
+   local statusString = dofile("httpserver-header.lc")(connection, args.code, "html", false, req.headers)
+   connection:send("<html><head><title>" .. args.code .. " - " .. statusString .. "</title></head><body><h1>" .. args.code .. " - " .. statusString .. "</h1></body></html>\r\n")
 end
