34329616398b90ad59584291f56b60f35c165890
Marcos Kirsch <github@kirsch.com.mx>
2017-07-01 15:14:37 -0500

    Restore defaults

--- httpserver-conf.lua
+++ httpserver-conf.lua
@@ -9,9 +9,9 @@ local auth = {}
 -- Set to true if you want to enable.
 auth.enabled = false
 -- Displayed in the login dialog users see before authenticating.
-auth.realm = "kirsch-garage"
+auth.realm = "nodemcu-httpserver"
 -- Add users and passwords to this table. Do not leave this unchanged if you enable authentication!
-auth.users = {kirsch = "poVOwOgI"}
+auth.users = {user1 = "password1", user2 = "password2", user3 = "password3"}
 
 conf.auth = auth
 return conf
