d11c494364ba630d4f051ef6147156500aaca29b
Godzil <godzil@godzil.net>
2017-09-03 23:16:58 +0100

    now user proper table based parameter for wifi.ap.setip instead of a string

--- httpserver-conf.lua
+++ httpserver-conf.lua
@@ -20,9 +20,9 @@ if (conf.wifi.mode == wifi.SOFTAP) or (conf.wifi.mode == wifi.STATIONAP) then
    conf.wifi.accessPoint.config = {}
    conf.wifi.accessPoint.config.ssid = "ESP-"..node.chipid() -- Name of the WiFi network to create.
    conf.wifi.accessPoint.config.pwd = "ESP-"..node.chipid() -- WiFi password for joining - at least 8 characters
-   conf.wifi.accessPoint.ip = "192.168.111.1"
---   conf.wifi.accessPoint.netmask = "255.255.255.0"
---   conf.wifi.accessPoint.gateway = "192.168.111.1"
+   conf.wifi.accessPoint.net.ip = "192.168.111.1"
+   conf.wifi.accessPoint.net.netmask="255.255.255.0"
+   conf.wifi.accessPoint.net.gateway="192.168.111.1" }
 end
 -- These apply only when connecting to a router as a client
 if (conf.wifi.mode == wifi.STATION) or (conf.wifi.mode == wifi.STATIONAP) then
