6c7e451663080e68b72d00685d324be55b3ba4fe
Manoël Trapier <godzil@godzil.net>
2019-11-14 18:01:01 +0000

    Change text/plain to application/octet-stream (#126)

--- httpserver-header.lua
+++ httpserver-header.lua
@@ -15,7 +15,7 @@ return function(connection, code, extension, isGzipped, extraHeaders)
       -- A few MIME types. Keep list short. If you need something that is missing, let's add it.
       local mt = {css = "text/css", gif = "image/gif", html = "text/html", ico = "image/x-icon", jpeg = "image/jpeg", 
          jpg = "image/jpeg", js = "application/javascript", json = "application/json", png = "image/png", xml = "text/xml"}
-      if mt[ext] then return mt[ext] else return "text/plain" end
+      if mt[ext] then return mt[ext] else return "application/octet-stream" end
    end
 
    local mimeType = getMimeType(extension)
